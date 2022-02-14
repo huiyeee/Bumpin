6673d0660963a47f90b751740aee0d4f952f988a
Yvan Chu <60163603+yvanchu@users.noreply.github.com>
2022-02-14 17:35:28 -0600

    added prettier, eslint, and firebase in package.json

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+const App = () => {
+  [matched, setMatched] = useState(false);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Welcome to Bumpin! You're in the hallway</p>
+        {matched ? (
+          <a
+            className="App-link"
+            href="https://northwestern.zoom.us/my/yvanchu"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            You've bumped into someone! Click to join via Zoom
+          </a>
+        ) : (
+          <p>The hallway is empty... we'll let you know if someone comes in</p>
+        )}
       </header>
     </div>
   );
-}
+};
 
 export default App;
