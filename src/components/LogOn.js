ad7000d4e2d942d3ed34d49d562b7bd4105393c4
Yvan Chu <60163603+yvanchu@users.noreply.github.com>
2022-02-16 14:55:05 -0600

    use url/abc to set meeting id as abc for that person

--- src/components/LogOn.js
+++ src/components/LogOn.js
@@ -1,33 +1,23 @@
 import React from "react";
-import { Router, Routes, Route, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { signInWithGoogle } from "../utilities/firebase";
 import { Button, Typography } from "@mui/material";
 
 const LogOn = () => {
   const { meetingId } = useParams();
   return (
-    <Router>
-      <p>test</p>
-      <Routes>
-        <Route
-          path="/:meetingId"
-          component={
-            <div className="App">
-              <header className="App-header">
-                <Typography variant="h4">
-                  Welcome to Bumpin! Ready to get started? Your meeting id is
-                  {meetingId}.
-                </Typography>
-                <br />
-                <Button variant="contained" onClick={() => signInWithGoogle()}>
-                  Sign In
-                </Button>
-              </header>
-            </div>
-          }
-        ></Route>
-      </Routes>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <Typography variant="h4">
+          Welcome to Bumpin! Ready to get started? Your meeting id is
+          {meetingId}.
+        </Typography>
+        <br />
+        <Button variant="contained" onClick={() => signInWithGoogle()}>
+          Sign In
+        </Button>
+      </header>
+    </div>
   );
 };
 
