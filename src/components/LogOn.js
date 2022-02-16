0009a32e1833a6f74eaa0f3a70d72384fcc20f46
Yvan Chu <60163603+yvanchu@users.noreply.github.com>
2022-02-16 12:20:58 -0600

    still doesn't work, but I am trying to use react router useParams

--- src/components/LogOn.js
+++ src/components/LogOn.js
@@ -1,20 +1,33 @@
 import React from "react";
+import { Router, Routes, Route, useParams } from "react-router-dom";
 import { signInWithGoogle } from "../utilities/firebase";
 import { Button, Typography } from "@mui/material";
 
 const LogOn = () => {
+  const { meetingId } = useParams();
   return (
-    <div className="App">
-      <header className="App-header">
-        <Typography variant="h4">
-          Welcome to Bumpin! Ready to get started?
-        </Typography>
-        <br />
-        <Button variant="contained" onClick={() => signInWithGoogle()}>
-          Sign In
-        </Button>
-      </header>
-    </div>
+    <Router>
+      <p>test</p>
+      <Routes>
+        <Route
+          path="/:meetingId"
+          component={
+            <div className="App">
+              <header className="App-header">
+                <Typography variant="h4">
+                  Welcome to Bumpin! Ready to get started? Your meeting id is
+                  {meetingId}.
+                </Typography>
+                <br />
+                <Button variant="contained" onClick={() => signInWithGoogle()}>
+                  Sign In
+                </Button>
+              </header>
+            </div>
+          }
+        ></Route>
+      </Routes>
+    </Router>
   );
 };
 
