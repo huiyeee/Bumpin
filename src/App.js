501623cf998c9cdf1b7b344392705a8e594b5bb8
alejandrodavidmalavet <61026152+alejandrodavidmalavet@users.noreply.github.com>
2022-02-17 19:45:53 -0600

    Cleanup from swarm added sign out

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import {
-  useData,
-  setData,
-  signInWithGoogle,
-  useUserState,
-} from "./utilities/firebase";
+import { useData, setData, useUserState } from "./utilities/firebase";
 import { useParams } from "react-router-dom";
-import { Typography } from "@mui/material";
 
 import LogOn from "./components/LogOn";
 import LobbyPanel from "./components/Lobby";
@@ -21,45 +15,59 @@ import {
   WaitForConfirmation,
 } from "./utilities/constant";
 import MatchingPanel from "./components/MatchingPanel";
-import WaitingPanel from "./components/WaitingPanel";
+import { Button, Typography } from "@mui/material";
+import { signOut } from "./utilities/firebase";
 
 const App = () => {
   const [users, loading, error] = useData("/users");
   const { meetingId } = useParams();
   const [user] = useUserState();
-  const [userStatusInHallway, setUserStatusInHallway] = useState(Initial);
-
-  const toggleMatched = () => {
-    setMatched(!matched);
-  };
 
   useEffect(() => {
     if (user && meetingId) {
       setData(`/users/${user.uid}/previous_meeting_id`, meetingId);
-      setData(`/users/${user.uid}/status`, userStatusInHallway);
-      
+      setData(`/users/${user.uid}/status`, Initial);
     }
   }, [user]);
 
   if (error) return <h1>{error}</h1>;
   if (loading) return <h1>Loading Bumpin...</h1>;
 
-  if (user) {
-    if (!users[user.uid].zoom_link) {
-      return <AddZoomInfoPanel uid={user.uid}></AddZoomInfoPanel>;
+  const GetPage = () => {
+    if (user) {
+      if (!users[user.uid].zoom_link) {
+        return <AddZoomInfoPanel uid={user.uid}></AddZoomInfoPanel>;
+      } else if (users[user.uid].status === Initial) {
+        return <LobbyPanel uid={user.uid}></LobbyPanel>;
+      } else if (users[user.uid].status === Matching) {
+        return <MatchingPanel uid={user.uid} users={users}></MatchingPanel>;
+      } else if (users[user.uid].status === Matched) {
+        return (
+          <MatchedPanel
+            uid={user.uid}
+            shared_zoom_link={users[user.uid].shared_zoom_link}
+          ></MatchedPanel>
+        );
+      }
     }
-    else if (users[user.uid].status === Initial) {
-      return <LobbyPanel uid={user.uid}></LobbyPanel>;
-    } 
-    else if (users[user.uid].status === Matching){
-      return <MatchingPanel uid={user.uid} users={users}></MatchingPanel>;
-    }
-    else if (users[user.uid].status === Matched) {
-      return <MatchedPanel uid={user.uid} shared_zoom_link={users[user.uid].shared_zoom_link}></MatchedPanel>;
-    }
-  }
+    return <LogOn />;
+  };
 
-  return <LogOn />;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        {GetPage()}
+        {user ? (
+          <Button variant="contained" onClick={() => signOut()}>
+            Sign Out
+          </Button>
+        ) : (
+          <></>
+        )}
+      </header>
+    </div>
+  );
 };
 
 export default App;
--- src/components/MatchingPanel.js
+++ src/components/MatchingPanel.js
@@ -1,33 +1,41 @@
-import React, {useEffect} from "react";
-import {Button} from "@mui/material";
-import {setData} from "../utilities/firebase";
-import {Initial, Matched, Matching} from "../utilities/constant";
+import React, { useEffect } from "react";
+import { Button } from "@mui/material";
+import { setData } from "../utilities/firebase";
+import { Initial, Matched, Matching } from "../utilities/constant";
 
-const MatchingPanel = ({uid, users}) => {
-    useEffect(() => {
-            var filtered_items = Object.keys(users).filter( (key) => users[key].status === Matching &&  users[key].previous_meeting_id !== users[uid].previous_meeting_id)
-            console.log(filtered_items.length)
-            if (filtered_items.length !== 0){
-                console.log('can not be here')
-                // set zoom to the first person render with match panel first person zoom id
-                setData(`/users/${uid}/shared_zoom_link`, users[uid].zoom_link);
-                setData(`/users/${filtered_items[0]}/shared_zoom_link`, users[uid].zoom_link);
-
-                // set status to Matched
-                setData(`/users/${uid}/status`, Matched);
-                setData(`/users/${filtered_items[0]}/status`, Matched);
-            }
-        }
-        ,)
-    return (
-        <div>
-            <p>Matching</p>
-            <Button variant="contained" onClick={() => {
-                setData(`/users/${user.uid}/status`, Initial);
-            }}>
-                Leave the hallway
-            </Button>
-        </div>
+const MatchingPanel = ({ uid, users }) => {
+  useEffect(() => {
+    var filtered_items = Object.keys(users).filter(
+      (key) =>
+        users[key].status === Matching &&
+        users[key].previous_meeting_id !== users[uid].previous_meeting_id
     );
+    if (filtered_items.length !== 0) {
+      console.log("can not be here");
+      // set zoom to the first person render with match panel first person zoom id
+      setData(`/users/${uid}/shared_zoom_link`, users[uid].zoom_link);
+      setData(
+        `/users/${filtered_items[0]}/shared_zoom_link`,
+        users[uid].zoom_link
+      );
+
+      // set status to Matched
+      setData(`/users/${uid}/status`, Matched);
+      setData(`/users/${filtered_items[0]}/status`, Matched);
+    }
+  });
+  return (
+    <div>
+      <p>Matching</p>
+      <Button
+        variant="contained"
+        onClick={() => {
+          setData(`/users/${uid}/status`, Initial);
+        }}
+      >
+        Leave the hallway
+      </Button>
+    </div>
+  );
 };
 export default MatchingPanel;
