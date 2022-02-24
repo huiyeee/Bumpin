68322775eacfdf0e0e114d2709ede30325457949
Andrew <andrew.sf.liao@gmail.com>
2022-02-24 13:36:34 -0600

    adding logo to login page

--- src/components/LogOn.js
+++ src/components/LogOn.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { signInWithGoogle } from "../utilities/firebase";
 import { Button } from "@mui/material";
+import bumpinLogo from '../bumpinlogo.png';
 
 const LogOnPanel = () => {
   return (
     <div>
       <p>Welcome to Bumpin! Log in to start.</p>
+      <img src={bumpinLogo} alt="Bumpin logo" className="center" style={{
+          borderRadius: "50%",
+          width: 100,
+          height: 100,
+          background: "white",
+        }}/>
+        <p></p>
       <Button onClick={() => signInWithGoogle()}>Sign In</Button>
     </div>
   );
