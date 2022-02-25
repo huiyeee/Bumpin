b6d531ca478f499b544adecfde48afeb151239ba
Yvan Chu <60163603+yvanchu@users.noreply.github.com>
2022-02-25 11:45:14 -0600

    styling changes

--- src/App.js
+++ src/App.js
@@ -90,15 +90,12 @@ const App = () => {
 
   const background = () => {
     let url = "";
-    if (
-      user !== null &&
-      (users[user.uid].status === Initial || users[user.uid].status === Profile)
-    ) {
-      url =
-        "https://firebasestorage.googleapis.com/v0/b/bumpin-7d62f.appspot.com/o/background.png?alt=media&token=b35f2139-c32b-45ac-a713-1a194bae351e";
-    } else {
+    if (user !== null && users[user.uid].status === Matching) {
       url =
         "https://firebasestorage.googleapis.com/v0/b/bumpin-7d62f.appspot.com/o/hallway.png?alt=media&token=eec8653d-af5b-41d7-9f51-8ec4a73cdeaf";
+    } else {
+      url =
+        "https://firebasestorage.googleapis.com/v0/b/bumpin-7d62f.appspot.com/o/background.png?alt=media&token=b35f2139-c32b-45ac-a713-1a194bae351e";
     }
     return {
       backgroundImage: "url(" + url + ")",
