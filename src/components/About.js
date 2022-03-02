15df7cedf67675859fdf3022ffaa74bdec1e366e
Andrew <andrew.sf.liao@gmail.com>
2022-03-01 18:03:10 -0600

    adding a deafult about page component

--- src/components/About.js
+++ src/components/About.js
@@ -1,25 +1,29 @@
 import React from "react";
 import bumpinLogo from "../bumpinlogo.png";
+import { ThemeProvider, CssBaseline} from "@material-ui/core";
+import theme from "../styling/theme";
+import "../App.css";
 
 const AboutPanel = () => {
   return (
-    <div>
-      <img
-        src={bumpinLogo}
-        alt="Bumpin logo"
-        className="center"
-        style={{
-          borderRadius: "50%",
-          width: "15%",
-          height: "15%",
-          background: "white",
-          border: "7px solid #393555",
-          margin: "20px"
-        }}
-      />
-      <p></p>
-    </div>
+    <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="App" style={background()}>
+            <header className="App-header">About Bumpin</header>
+            <main className="App-main"><h1>Hi! Do you want to meet more of your coworkers? You can try out Bumpin app!</h1></main>
+        </div>
+    </ThemeProvider>
   );
 };
+
+const background = () => {
+    let url = "https://firebasestorage.googleapis.com/v0/b/bumpin-7d62f.appspot.com/o/background.png?alt=media&token=b35f2139-c32b-45ac-a713-1a194bae351e";
+    return {
+      backgroundImage: "url(" + url + ")",
+      backgroundPosition: "center",
+      backgroundSize: "cover",
+      backgroundRepeat: "no-repeat",
+    };
+  };
 
 export default AboutPanel;
