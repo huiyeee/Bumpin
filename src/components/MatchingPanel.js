52b61e6e42f853befd24d388411c6ca18057a9e6
Yvan Chu <60163603+yvanchu@users.noreply.github.com>
2022-03-07 17:34:53 -0600

    implemented matching filters by room preferences

--- src/components/MatchingPanel.js
+++ src/components/MatchingPanel.js
@@ -3,16 +3,17 @@ import { Button } from "@mui/material";
 import { setData } from "../utilities/firebase";
 import { Initial, Matched, Matching } from "../utilities/constant";
 import MatchableCard from "./MatchableCard";
-import ExitToAppIcon from '@mui/icons-material/ExitToApp';
-import DirectionsWalkIcon from '@mui/icons-material/DirectionsWalk';
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import DirectionsWalkIcon from "@mui/icons-material/DirectionsWalk";
 
 const MatchingPanel = ({ uid, users, setHeaderText }) => {
   const [matchIndex, setMatchIndex] = useState(0);
   const matches = Object.keys(users).filter(
     (key) =>
       users[key].status === Matching &&
-      users[key].previous_meeting_id !== users[uid].previous_meeting_id && 
-      users[key].team !== users[uid].team
+      users[key].previous_meeting_id !== users[uid].previous_meeting_id &&
+      users[key].team !== users[uid].team &&
+      users[key].roomPreference === users[uid].roomPreference
   );
   const showMatches = () => {
     if (matches.length == matchIndex) {
@@ -48,10 +49,12 @@ const MatchingPanel = ({ uid, users, setHeaderText }) => {
           className="b-button mui"
           onClick={() => {
             console.log("Previous match index: " + matchIndex);
-            setMatchIndex(matchIndex + 2 >= matches.length ? 0 : matchIndex + 2);
+            setMatchIndex(
+              matchIndex + 2 >= matches.length ? 0 : matchIndex + 2
+            );
           }}
         >
-          <DirectionsWalkIcon/>
+          <DirectionsWalkIcon />
           Keep walking
         </Button>
         <Button
@@ -60,7 +63,7 @@ const MatchingPanel = ({ uid, users, setHeaderText }) => {
             setData(`/users/${uid}/status`, Initial);
           }}
         >
-          <ExitToAppIcon/>
+          <ExitToAppIcon />
           Leave the hallway
         </Button>
       </div>
--- src/components/RoomSelected.js
+++ src/components/RoomSelected.js
@@ -3,21 +3,28 @@ import { Button } from "@mui/material";
 import { setData } from "../utilities/firebase";
 import { Matching } from "../utilities/constant";
 
-const RoomSelected = ({uid}) => {
+const RoomSelected = ({ uid }) => {
   return (
     <>
-        <Button className="b-button mui" onClick={() => {
-            setData(`/users/${uid}/roomPreference`, "game");
-        }}>
-            Enter the game room
-        </Button>
+      <Button
+        className="b-button mui"
+        onClick={() => {
+          setData(`/users/${uid}/status`, Matching);
+          setData(`/users/${uid}/roomPreference`, "game");
+        }}
+      >
+        Enter the game room
+      </Button>
 
-        <Button className="b-button mui" onClick={() => {
-            setData(`/users/${uid}/status`, Matching);
-            setData(`/users/${uid}/roomPreference`, "coffee");
-        }}>
-            Enter the coffee room
-        </Button>
+      <Button
+        className="b-button mui"
+        onClick={() => {
+          setData(`/users/${uid}/status`, Matching);
+          setData(`/users/${uid}/roomPreference`, "coffee");
+        }}
+      >
+        Enter the coffee room
+      </Button>
     </>
   );
 };
