87d750e48ea2d1b8df02a8ea79498dae51111654
Yvan Chu <60163603+yvanchu@users.noreply.github.com>
2022-03-07 17:35:20 -0600

    team status and previous meeting id doesn't matter

--- src/components/MatchingPanel.js
+++ src/components/MatchingPanel.js
@@ -11,8 +11,8 @@ const MatchingPanel = ({ uid, users, setHeaderText }) => {
   const matches = Object.keys(users).filter(
     (key) =>
       users[key].status === Matching &&
-      users[key].previous_meeting_id !== users[uid].previous_meeting_id &&
-      users[key].team !== users[uid].team &&
+      // users[key].previous_meeting_id !== users[uid].previous_meeting_id &&
+      // users[key].team !== users[uid].team &&
       users[key].roomPreference === users[uid].roomPreference
   );
   const showMatches = () => {
