77d6799bfaa014f8b1c020b312baa1d4c773ea0e
alejandrodavidmalavet <61026152+alejandrodavidmalavet@users.noreply.github.com>
2022-03-10 20:15:41 -0600

    Update Meeting.js

--- src/components/Meeting/Meeting.js
+++ src/components/Meeting/Meeting.js
@@ -235,36 +235,9 @@ const MeetingChat = ({ tollbarHeight }) => {
 const ParticipantView = ({ participantId }) => {
   const webcamRef = useRef(null);
   const micRef = useRef(null);
-  const screenShareRef = useRef(null);
 
-  const onStreamEnabled = (stream) => {};
-  const onStreamDisabled = (stream) => {};
-
-  const {
-    displayName,
-    participant,
-    webcamStream,
-    micStream,
-    screenShareStream,
-    webcamOn,
-    micOn,
-    screenShareOn,
-    isLocal,
-    isActiveSpeaker,
-    isMainParticipant,
-    switchTo,
-    pinState,
-    setQuality,
-    enableMic,
-    disableMic,
-    enableWebcam,
-    disableWebcam,
-    pin,
-    unpin,
-  } = useParticipant(participantId, {
-    onStreamEnabled,
-    onStreamDisabled,
-  });
+  const { displayName, webcamStream, micStream, webcamOn, micOn, isLocal } =
+    useParticipant(participantId);
 
   useEffect(() => {
     if (webcamRef.current) {
@@ -302,37 +275,14 @@ const ParticipantView = ({ participantId }) => {
     }
   }, [micStream, micOn]);
 
-  useEffect(() => {
-    if (screenShareRef.current) {
-      if (screenShareOn) {
-        const mediaStream = new MediaStream();
-        mediaStream.addTrack(screenShareStream.track);
-
-        screenShareRef.current.srcObject = mediaStream;
-        screenShareRef.current
-          .play()
-          .catch((error) =>
-            console.error("videoElem.current.play() failed", error)
-          );
-      } else {
-        screenShareRef.current.srcObject = null;
-      }
-    }
-  }, [screenShareStream, screenShareOn]);
-
   return (
     <div
       style={{
-        width: "97%",
-        backgroundColor: primary,
-        borderRadius: borderRadius,
+        width: "100%",
         overflow: "hidden",
-        margin: borderRadius,
-        padding: borderRadius,
         display: "flex",
         flexDirection: "column",
-        position: "relative",
-        height: "43vh",
+        height: "45vh",
       }}
     >
       <audio ref={micRef} autoPlay muted={isLocal} />
@@ -384,7 +334,17 @@ const ParticipantView = ({ participantId }) => {
             </p>
             <p
               style={{
-                color: webcamOn ? "green" : "red",
+                color: micOn ? "green" : "red",
+                fontSize: 16,
+                fontWeight: "bold",
+                opacity: 1,
+              }}
+            >
+              MIC
+            </p>
+            <p
+              style={{
+                color: "white",
                 fontSize: 16,
                 fontWeight: "bold",
                 opacity: 1,
@@ -393,117 +353,8 @@ const ParticipantView = ({ participantId }) => {
               {displayName}
             </p>
           </div>
-
-          <div
-            style={{
-              position: "absolute",
-              top: 10,
-              left: 10,
-            }}
-          >
-            {/* <button
-              className="button blue"
-              style={
-                {
-                  // height: 50,
-                  // width: 200,
-                }
-              }
-              onClick={async () => {
-                const meetingId = prompt(
-                  `Please enter meeting id where you want to switch ${displayName}`
-                );
-                const token = await getToken();
-                if (meetingId && token) {
-                  try {
-                    await switchTo({
-                      meetingId,
-                      payload: "Im Switching",
-                      token: token,
-                    });
-                  } catch (e) {
-                    console.log("swithc To Error", e);
-                  }
-                } else {
-                  alert("Empty meetingId!");
-                }
-              }}
-            >
-              Switch Participant
-            </button> */}
-          </div>
         </div>
       </div>
-
-      {/* <div
-        style={{
-          marginTop: borderRadius,
-          position: "relative",
-          borderRadius: borderRadius,
-          overflow: "hidden",
-          backgroundColor: "lightgreen",
-          width: "100%",
-          height: 300,
-        }}
-      >
-        <div
-          style={{ position: "absolute", top: 0, left: 0, right: 0, bottom: 0 }}
-        >
-          <video
-            height={"100%"}
-            width={"100%"}
-            ref={screenShareRef}
-            style={{
-              backgroundColor: "black",
-              position: "absolute",
-              top: 0,
-              left: 0,
-              right: 0,
-              bottom: 0,
-              objectFit: "contain",
-            }}
-            autoPlay
-          />
-          <div
-            style={{
-              position: "absolute",
-              top: borderRadius,
-              right: borderRadius,
-            }}
-          >
-            <p
-              style={{
-                color: screenShareOn ? "green" : "red",
-                fontSize: 16,
-                fontWeight: "bold",
-                opacity: 1,
-              }}
-            >
-              SCREEN SHARING
-            </p>
-          </div>
-        </div>
-      </div> */}
-      {/* <table>
-        {[
-          { k: "Name", v: displayName },
-          { k: "webcamOn", v: webcamOn ? "YES" : "NO" },
-          { k: "micOn", v: micOn ? "YES" : "NO" },
-          { k: "screenShareOn", v: screenShareOn ? "YES" : "NO" },
-          { k: "isLocal", v: isLocal ? "YES" : "NO" },
-          { k: "isActiveSpeaker", v: isActiveSpeaker ? "YES" : "NO" },
-          { k: "isMainParticipant", v: isMainParticipant ? "YES" : "NO" },
-        ].map(({ k, v }) => (
-          <tr key={k}>
-            <td style={{ border: "1px solid #fff", padding: 4 }}>
-              <h3 style={{ margin: 0, padding: 0, color: "#fff" }}>{k}</h3>
-            </td>
-            <td style={{ border: "1px solid #fff", padding: 4 }}>
-              <h3 style={{ margin: 0, padding: 0, color: "#fff" }}>{v}</h3>
-            </td>
-          </tr>
-        ))}
-      </table> */}
     </div>
   );
 };
